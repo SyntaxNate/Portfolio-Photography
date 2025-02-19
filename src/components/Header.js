5e8c0ea1291e8b140be64c3e32f9794d53ca5e8f
SyntaxNate <new-shutter.nbt@gmail.com>
2025-02-18 22:54:04 -0500

    animation works

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,29 @@
 import React from "react";
-import "./Hero.css"
+import "./Hero.css";
 
 const Header = () => {
 
     return (
 
-    <header className="w-full py-6 fixed top-0 left-0 z-10 bg-gradient-to-r 
-                    from-[#574e45] to-[#b08968]">
-        <nav className="container mx-auto flex flex-col items-center">
-                <h1 className="text-white text-3xl mb-2 uppercase tracking-wide">
-                    Acuated Clarity
-                </h1>
-                <ul className="w-full flex justify-between text-white text-lg tracking-wide">
-                    {["Home", "Gallery", "About", "Contact"].map((item) => (
-                        <li key={item} className="relative group">
-                            <a href={`#${item.toLowerCase()}`}
-                                className="relative inline-block px-4 py-2 transition-all
-                                            duration-300 ease-in-out transform 
-                                            group-hover:scale-110">
-                        
-                            <span className="relative z-10">
-                                {item}</span>
-                            <span className="absolute inset-0 left-[100%] bg-white opacity-0 
-                                            group-hover:opacity-30 group-hover:left-[-100%]
-                                            transition-all duration-500 ease-out skew-x-[-20deg]">
-                            </span>
-                            </a>
-                       </li>
-
-                    ))}
-                    
-                    
-                </ul>
-        </nav>
-
-    </header>
+        <header className="w-full py-6 fixed top-0 left-0 z-10 bg-gradient-to-r from-[#574e45] to-[#b08968]">
+    <nav className="container mx-auto flex flex-col items-center">
+        <h1 className="text-white text-3xl mb-2 uppercase tracking-wide">
+            Acuated Clarity
+        </h1>
+        <ul className="w-full flex justify-between text-white text-lg tracking-wide">
+            {["Home", "Gallery", "About", "Contact"].map((item) => (
+                <li key={item} className="relative">
+                    <a 
+                        href={`#${item.toLowerCase()}`}
+                        className="nav-link relative inline-block px-4 py-2 transform transition-all duration-300 ease-in-out group hover:scale-110"
+                    >
+                        {item}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </nav>
+</header>
         
     )
 }
--- src/components/Heropage.js
+++ src/components/Heropage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Hero.css";
+import "../index.css";
 import Img1 from "../assets/Img1.png";
 import Img2 from "../assets/Img2.png";
 import Img3 from "../assets/Img3.png";
